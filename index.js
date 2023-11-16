6eb51035fb983b37cbe68c6de6dd7c0cc601205b
wanpp <6653302wanpp@gmail.com>
2023-11-16 15:42:44 +0800

    feat: add tencent api

--- index.js
+++ index.js
@@ -2,106 +2,124 @@
 const axios = require("axios");
 const sha256 = require("sha256");
 const utf8 = require("utf8");
+const tencnet = require("./tencent");
 
-const cmdargs = [
-  "",
-  "",
-  "无法识别要翻译的内容，请重新输入",
-  // "-f",
-  // "en",
-  // "-t",
-  // "zh-CHT",
-]; // process.argv;
-console.log("cmdargs: ", process.argv);
+const cmdargs = process.argv;
+// const cmdargs = [
+//   "",
+//   "",
+//   "无法识别要翻译的内容，请重新输入",
+//   // "-f",
+//   // "en",
+//   // "-t",
+//   // "zh",
+// ];
+// console.log("cmdargs: ",  cmdargs);
 let query = cmdargs?.[2] ?? ""; // 获取命令行参数
 if (!query) {
   console.log("warning: 无法识别要翻译的内容，请重新输入");
   return;
 }
-if (query.length > 200) query = query.slice(0, 200); // 不能超xx个字符
+if (query.length > 2000) query = query.slice(0, 2000); // 不能超xx个字符
 
-const showResult = (data) => {
-  let result = "\n" + data.query + ": " + data.translation.join(",");
-  if (data?.basic) {
-    result += "\n" + data.basic.explains.join("\n");
-  }
-  if (data?.web) {
-    result += "\n\n" + "网络释义: ";
-    let webStr = "";
-    data.web.forEach((item) => {
-      webStr += "\n" + `${item.key}: ${item.value.join(",")}`;
-    });
-    result += webStr;
-  }
+// const showResult = (data) => {
+//   let result = "\n" + data.query + ": " + data.translation.join(",");
+//   if (data?.basic) {
+//     result += "\n" + data.basic.explains.join("\n");
+//   }
+//   if (data?.web) {
+//     result += "\n\n" + "网络释义: ";
+//     let webStr = "";
+//     data.web.forEach((item) => {
+//       webStr += "\n" + `${item.key}: ${item.value.join(",")}`;
+//     });
+//     result += webStr;
+//   }
 
-  console.log(result);
+//   console.log(result);
+// };
+
+const iszh = (str) => {
+  return /[\u4e00-\u9fa5|\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]/.test(
+    str
+  );
 };
 
 const checkLan = () => {
-  let from = "";
+  // 检测输入的文本是否是中文
+  const querySource = iszh(query) ? "zh" : "en";
+
+  let from = querySource;
   const hasfromkey = cmdargs.indexOf("-f");
   if (hasfromkey !== -1) {
     const fromlan = cmdargs?.[hasfromkey + 1];
     from = fromlan ? fromlan : "auto";
   }
 
-  let to = "";
+  let to = querySource === "zh" ? "en" : "zh";
   const hastokey = cmdargs.indexOf("-t");
   if (hastokey !== -1) {
     const tolan = cmdargs?.[hastokey + 1];
-    if (!tolan) {
-      // 检测要翻译的文字是中文/英文，设置目标语言为英文/中文
-      const iszh =
-        /^(?:[\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\uD840-\uD868\uD86A-\uD86C\uD86F-\uD872\uD874-\uD879][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D\uDC20-\uDFFF]|\uD873[\uDC00-\uDEA1\uDEB0-\uDFFF]|\uD87A[\uDC00-\uDFE0])+$/.test(
-          str
-        );
-      to = iszh ? "en" : "zh-CHS";
-    } else to = tolan;
+    to = tolan ? tolan : to;
   }
 
   return { from, to };
 };
 
-const url = "https://openapi.youdao.com/api";
-const appKey = "43c9579332d95d7f";
 const getTranslateStr = () => {
   if (!query) return;
 
   // query = utf8.decode(query);
-  const curtime = Math.round(new Date().getTime() / 1000);
-  const salt = new Date().getTime();
+  // const curtime = Math.round(new Date().getTime() / 1000);
+  // const salt = new Date().getTime();
   // input的计算方式为：input=q前10个字符 + q长度 + q后10个字符（当q长度大于20）或 input=q字符串（当q长度小于等于20）
-  const input =
-    query?.length > 20
-      ? `${query.substring(0, 10)}${query.length}${query.substring(
-          query.length - 10
-        )}`
-      : query;
-  console.log("input: ", input);
-  // sha256(应用ID+input+salt+curtime+应用密钥)
-  const sign = sha256(
-    `${appKey}${input}${salt}${curtime}Vh8UL4hyCMle9ULqX3YrV31t2CFG0H2w`
-  );
-  axios
-    .post(
-      url,
-      { q: query, appKey, salt, sign, signType: "v3", curtime, ...checkLan() }
-      // {
-      //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      // }
-    )
-    .then(function (response) {
-      const data = response.data;
-      if (data?.errorCode !== "0") {
-        console.log("error", response);
-      } else {
-        // console.log("response: ", data);
-        showResult(data);
-      }
-    })
-    .catch(function (error) {
-      console.log("error" + error);
-    });
+  // const input =
+  //   query?.length > 20
+  //     ? `${query.substring(0, 10)}${query.length}${query.substring(
+  //         query.length - 10
+  //       )}`
+  //     : query;
+  // console.log("input: ", input);
+  // // sha256(应用ID+input+salt+curtime+应用密钥)
+  // const sign = sha256(
+  //   `${appKey}${input}${salt}${curtime}Vh8UL4hyCMle9ULqX3YrV31t2CFG0H2w`
+  // );
+
+  const { from, to } = checkLan();
+
+  // 腾讯api
+  tencnet(query, from, to).then((result) => {
+    console.log(result);
+  });
+  // 腾讯api
+  // axios
+  //   .post(
+  //     url,
+  //     {
+  //       Action: "TextTranslate",
+  //       Version: "2018-03-21",
+  //       Region: "ap-shanghai",
+  //       SourceText: query,
+  //       Source: from,
+  //       Target: to,
+  //       ProjectId: 0,
+  //     }
+  //     // {
+  //     //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     // }
+  //   )
+  //   .then(function (response) {
+  //     const data = response.data;
+  //     if (data?.errorCode !== "0") {
+  //       console.log("error", response);
+  //     } else {
+  //       // console.log("response: ", data);
+  //       showResult(data);
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     console.log("error==: " + error);
+  //   });
 };
 
 getTranslateStr();
---
+++ tencent.js
@@ -0,0 +1,55 @@
+// Depends on tencentcloud-sdk-nodejs version 4.0.3 or higher
+const tencentcloud = require("tencentcloud-sdk-nodejs-tmt");
+
+const TmtClient = tencentcloud.tmt.v20180321.Client;
+
+// 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
+// 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
+// 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+const clientConfig = {
+  credential: {
+    secretId: "AKIDLYaQ2PVP6Zoogu3ylMm5BMUcgXCh1V7h",
+    secretKey: "hMxTq1xeAVs9D810xljjYDj4FrHgCaoz",
+  },
+  region: "ap-shanghai",
+  profile: {
+    httpProfile: {
+      endpoint: "tmt.tencentcloudapi.com",
+    },
+  },
+};
+
+// 实例化要请求产品的client对象,clientProfile是可选的
+const client = new TmtClient(clientConfig);
+const params = {
+  SourceText: "你好",
+  Source: "zh",
+  Target: "en",
+  ProjectId: 0,
+};
+
+const tencnet = (text, source, target) => {
+  return new Promise((resolve) => {
+    client
+      .TextTranslate({
+        ...params,
+        SourceText: text,
+        Source: source,
+        Target: target,
+      })
+      .then(
+        (data) => {
+          console.log(data);
+          resolve(data?.TargetText || "");
+        },
+        (err) => {
+          console.error("error", err);
+        }
+      );
+  });
+};
+
+// 测试用
+// tencnet();
+
+module.exports = tencnet;
